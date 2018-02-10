12de20e5c225d5165dd7ea532600d4af3073820c
jryzkns <xza194@sfu.ca>
2018-02-10 03:47:46 -0800

    main movement mechanics done!

--- MainUnity/Assets/PlayerBehaviour.cs
+++ MainUnity/Assets/PlayerBehaviour.cs
@@ -5,17 +5,21 @@ using System;
 
 public class PlayerBehaviour : MonoBehaviour {
 
+	public float offset = 0.0f;
+	public double angel = 0.0f;
 	// Use this for initialization
 	void Start () {
 		//item should start at (0,5,2)
+		angel = 0.0f;
+		transform.position = new Vector3(	(float)(5*System.Math.Cos(angel)), 	(float)(5*System.Math.Sin(angel)), 	0);
 	}
 	
-	void Turn(double angle, int x){
-
-		double newangle = angle + x*0.05;
+	void Turn(int x){
+		angel = angel + x*0.05;
+		//double newangle = angle + x*0.05;
 
 		//assuming that the turn radius is 5
-		transform.position = new Vector3(	(float)(5*System.Math.Cos(newangle)), 	(float)(5*System.Math.Sin(newangle)), 	0);
+		transform.position = new Vector3(	(float)(5*System.Math.Cos(angel)), 	(float)(5*System.Math.Sin(angel)), 	0);
 	}
 
 	void Change_Resolution(int i){
@@ -28,19 +32,27 @@ public class PlayerBehaviour : MonoBehaviour {
 		
 	}
 	
+	void Follow_Mouse(){
+		Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + offset - 90);
+	}
 
 	// Update is called once per frame
 	void Update () {
 
 		//computes angle wrt origin
-		double angle = System.Math.Atan((transform.position.y)/(transform.position.x));
+		//double angle = System.Math.Atan((transform.position.y)/(transform.position.x));
+
+		Follow_Mouse();
 
 		//listens to inputs
 		if (Input.GetKey("d")){
-			Turn(angle, 1);
+			Turn(1);
 		}
 		if (Input.GetKey("a")){
-			Turn(angle, -1);
+			Turn(-1);
 		}
 		if (Input.GetKeyDown("w")){
 			Change_Resolution(1);
