334ca622011fec99c7f0145640853573fa512ecb
gareth016 <gareth016@gmail.com>
2018-02-10 03:10:32 -0800

    Object points to mouse
    idk why it's also got like 200 files but idk i hope it doesn't break the build :')

---
+++ MainUnity/Assets/Pointtomouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointtomouse : MonoBehaviour
+{
+    public float offset = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + offset - 90);
+    }
+}
