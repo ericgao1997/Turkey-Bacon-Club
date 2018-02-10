8063974cc595537eb46e04a0d7d12b8421944d79
ericgao1997 <31947627+ericgao1997@users.noreply.github.com>
2018-02-10 11:23:28 -0800

    Bear is now in game kinda
    Sprite implemented

---
+++ MainUnity/Assets/bearSprite.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bearSprite : MonoBehaviour {
+	public GameObject follow;
+	// Use this for initialization
+	void Start () {
+		transform.position = follow.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = follow.transform.position;
+	}
+}
