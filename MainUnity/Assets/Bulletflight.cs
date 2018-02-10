e0ac0239f49bde6a8dd4b961cebe66554dc98bf1
jryzkns <xza194@sfu.ca>
2018-02-10 10:51:22 -0800

    added bullet animation and auto delete

---
+++ MainUnity/Assets/Bulletflight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletflight : MonoBehaviour {
+
+	int lifespan;
+
+	// Use this for initialization
+	void Start () {
+		Debug. Log("initialized");
+		lifespan=10;
+	}
+	
+	Vector3 xmove = new Vector3(0.5f,0f,0f);
+
+	// Update is called once per frame
+	void Update () {
+
+		//no collision case
+		if (lifespan > 0){
+			transform.position += xmove;
+			lifespan-=1;
+		} else {
+			//this.renderer.enabled = false;
+			GetComponent<Renderer>().enabled = false;
+			Destroy(this);
+		}
+		
+	}
+}
