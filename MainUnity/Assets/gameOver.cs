bb8867efcbc7043e208dc8eedeedfd75111f1066
Plasmalaser <fubof@sfu.ca>
2018-02-10 21:48:30 -0800

    hpManager and gameOver GameManager init setup

---
+++ MainUnity/Assets/gameOver.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameOver : MonoBehaviour {
+
+	public void endGame(){
+		Debug.Log ("Game Over");
+	}
+}
---
+++ MainUnity/Assets/hpManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hpManager : MonoBehaviour {
+
+	static public int health; //default 100
+
+	// Use this for initialization
+	void Start () {}
+
+	void OnCollisionEnter(Collision col){
+		if (col.collider.tag = "enemy") {
+			health -= 1;
+		}
+		if (health <= 0) {
+			FindObjectOfType<gameOver>().endGame();
+		}
+	}
+}
