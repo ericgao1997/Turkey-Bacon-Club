489233d62064a23dedfebf43053251ca10e35c75
ericgao1997 <31947627+ericgao1997@users.noreply.github.com>
2018-02-11 12:41:59 -0800

    Com

---
+++ MainUnity/Assets/Scripts/SkyboxRotator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SkyboxRotator : MonoBehaviour
+{
+    public float RotationPerSecond = 1;
+    private bool _rotate;
+    public Material[] Skyboxes;
+
+    protected void Update()
+    {
+        RenderSettings.skybox = Skyboxes[resChanger.level];
+        if (_rotate) RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotationPerSecond);
+    }
+
+    public void ToggleSkyboxRotation()
+    {
+        _rotate = !_rotate;
+    }
+}
