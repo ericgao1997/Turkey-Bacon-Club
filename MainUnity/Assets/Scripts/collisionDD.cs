3369a1cbcefc86e4ef1e9532afd4e33ac7581590
jryzkns <xza194@sfu.ca>
2018-02-10 19:59:01 -0800

    monster collision patched

--- MainUnity/Assets/Scripts/collisionDD.cs
+++ MainUnity/Assets/Scripts/collisionDD.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class collisionDD : MonoBehaviour {
 
-	void OncollisionEnter(){
-		GetComponent<Renderer> ().enabled = false;
-		Destroy (this);
-	}
+    void Start(){
+        //stuff
+    }
+
+    bool in_ring(int r){
+        return (    System.Math.Sqrt(
+                                        System.Math.Pow((transform.position.x),2) + 
+                                        System.Math.Pow((transform.position.y),2)
+                                    ) <= 
+                                        r
+                );
+    }
+
+    void Update(){
+        if (in_ring(5)){
+            Debug. Log ("In!");
+            gameObject.GetComponent<Renderer>().enabled = false;
+            Destroy(gameObject);
+        }
+    }
+
+    // void OncollisionEnter(){
+    //     GetComponent<Renderer> ().enabled = false;
+    //     Destroy (this);
+    // }
 }
