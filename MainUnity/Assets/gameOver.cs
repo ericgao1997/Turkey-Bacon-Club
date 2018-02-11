f0fc42e90659e28aaa0a7dd7fe3a62595ea0d252
Plasmalaser <fubof@sfu.ca>
2018-02-10 22:00:04 -0800

    restart call implemented in gameOver.cs

--- MainUnity/Assets/gameOver.cs
+++ MainUnity/Assets/gameOver.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class gameOver : MonoBehaviour {
 
+	bool gameHasEnded = false;
+
 	public void endGame(){
-		Debug.Log ("Game Over");
+		
+		if (gameHasEnded == false) {
+			gameHasEnded = true;
+			Debug.Log ("Game Over");
+			//restart game
+			Restart();
+		}
+
 	}
+
+	void Restart(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
 }
