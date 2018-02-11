815818267bff55eb18835bd77e30d19aafce56d9
ericgao1997 <31947627+ericgao1997@users.noreply.github.com>
2018-02-11 12:27:43 -0800

    Menu and Skybox

--- MainUnity/Assets/FarlandSkies/Demo/Scripts/Skybox/SkyboxRotator.cs
+++ MainUnity/Assets/FarlandSkies/Demo/Scripts/Skybox/SkyboxRotator.cs
@@ -4,10 +4,12 @@ public class SkyboxRotator : MonoBehaviour
 {
     public float RotationPerSecond = 1;
     private bool _rotate = true;
+    public Material[] Skyboxes;
 
     protected void Update()
     {
         if (_rotate) RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotationPerSecond);
+        RenderSettings.skybox = Skyboxes[resChanger.level];
     }
 
     public void ToggleSkyboxRotation()
