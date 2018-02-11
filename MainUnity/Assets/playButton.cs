7da173e7c65239fb4dab9718f4234bc660af9934
Plasmalaser <fubof@sfu.ca>
2018-02-10 23:18:25 -0800

    build with multiple scenes to make menu scene stuf work also wtf is this pixel crap

---
+++ MainUnity/Assets/playButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class playButton : MonoBehaviour {
+
+	/*
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		SceneManager.LoadScene ("Main");
+	}
+	*/
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			//Debug.Log ("Test");
+			SceneManager.LoadScene("Main"); //gotta build the app with both scenes(s)
+		}
+	}
+}
